7b1dc899788df29c985e4e0957ae3a304f942f2e
Kid Develop <kawee.l@g-able.com>
2017-03-01 18:12:38 +0700

    use new anno

--- KidsMongoWs/src/main/java/com/kids/ws/user/GetAllUserInfoRestController.java
+++ KidsMongoWs/src/main/java/com/kids/ws/user/GetAllUserInfoRestController.java
@@ -1,10 +1,11 @@
-package com.kids.ws;
+package com.kids.ws.user;
 
 import com.kids.dao.model.UserInfo;
 import com.kids.dao.repository.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class UserInfoRest {
+public class GetAllUserInfoRestController {
 
     @Autowired
     private UserInfoRepository userInfoRepository;
 
-    @RequestMapping(value = "getAllUser",method = RequestMethod.GET)
+    @GetMapping("getAllUser")
     public ResponseEntity<List<UserInfo>> doProcess(){
 
         List<UserInfo> responseObj = userInfoRepository.findAll();
-
-        System.out.println("MONGO DB");
 
         return new ResponseEntity<>(responseObj,HttpStatus.OK);
     }

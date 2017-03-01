59761eb101263fc286ef57741dc96e4d6d02afe0
Kid Develop <kawee.l@g-able.com>
2017-03-01 18:13:44 +0700

    create Model

---
+++ KidsMongoDao/src/main/java/com/kids/dao/model/ErrorInfo.java
@@ -0,0 +1,49 @@
+package com.kids.dao.model;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class ErrorInfo extends DefaultModel {
+
+    private static final long serialVersionUID = 5300770146236915183L;
+
+    @Indexed(unique = true)
+    private String errorCode;
+    private String errorMessageTh;
+    private String errorMessageEn;
+
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessageTh() {
+        return errorMessageTh;
+    }
+
+    public void setErrorMessageTh(String errorMessageTh) {
+        this.errorMessageTh = errorMessageTh;
+    }
+
+    public String getErrorMessageEn() {
+        return errorMessageEn;
+    }
+
+    public void setErrorMessageEn(String errorMessageEn) {
+        this.errorMessageEn = errorMessageEn;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() + "ErrorInfo{" +
+                "errorCode='" + errorCode + '\'' +
+                ", errorMessageTh='" + errorMessageTh + '\'' +
+                ", errorMessageEn='" + errorMessageEn + '\'' +
+                '}';
+    }
+}

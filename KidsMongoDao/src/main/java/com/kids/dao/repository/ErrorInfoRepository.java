59b3c43885cf67793ce6cee581d37bae75a931f5
Kid Develop <kawee.l@g-able.com>
2017-03-01 18:16:03 +0700

    create repo

---
+++ KidsMongoDao/src/main/java/com/kids/dao/repository/ErrorInfoRepository.java
@@ -0,0 +1,20 @@
+package com.kids.dao.repository;
+
+import com.kids.dao.model.ErrorInfo;
+import com.mongodb.WriteResult;
+
+import java.util.List;
+
+public interface ErrorInfoRepository {
+
+    List<ErrorInfo> findAll();
+
+    ErrorInfo findByErrorCode(String errorCode);
+
+    ErrorInfo update(ErrorInfo errorInfo);
+
+    void save(ErrorInfo errorInfo);
+
+    void delete(ErrorInfo errorInfo);
+
+}
---
+++ KidsMongoDao/src/main/java/com/kids/dao/repository/ErrorInfoRepositoryImpl.java
@@ -0,0 +1,66 @@
+package com.kids.dao.repository;
+
+import com.kids.dao.model.ErrorInfo;
+import com.mongodb.WriteResult;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ErrorInfoRepositoryImpl implements ErrorInfoRepository {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+    @Override
+    public List<ErrorInfo> findAll() {
+        return mongoTemplate.findAll(ErrorInfo.class);
+    }
+
+    @Override
+    public ErrorInfo findByErrorCode(String errorCode) {
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("errorCode").is(errorCode));
+
+        return mongoTemplate.findOne(query,ErrorInfo.class);
+    }
+
+    @Override
+    public ErrorInfo update(ErrorInfo errorInfo) {
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id").is(errorInfo.getId()));
+
+        FindAndModifyOptions options = new FindAndModifyOptions();
+        options.returnNew(true);
+
+        Update update = new Update();
+        update.set("errorCode",errorInfo.getErrorCode());
+        update.set("errorMessageTh",errorInfo.getErrorMessageTh());
+        update.set("errorMessageEn",errorInfo.getErrorMessageEn());
+        update.set("modifyDate", errorInfo.getModifyDate());
+        update.set("modifyBy", errorInfo.getModifyBy());
+
+        return mongoTemplate.findAndModify(query,update,options,ErrorInfo.class);
+    }
+
+    @Override
+    public void save(ErrorInfo errorInfo) {
+        mongoTemplate.insert(errorInfo);
+    }
+
+    @Override
+    public void delete(ErrorInfo errorInfo) {
+        mongoTemplate.remove(errorInfo);
+    }
+
+}

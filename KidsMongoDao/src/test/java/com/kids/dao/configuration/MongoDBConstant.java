d87f2e7c5679a36c010a37a41dfcff49beea5197
Kid Develop <kawee.l@g-able.com>
2017-03-01 18:16:32 +0700

    config & test repository

---
+++ KidsMongoDao/src/test/java/com/kids/dao/configuration/MongoDBConstant.java
@@ -0,0 +1,8 @@
+package com.kids.dao.configuration;
+
+public final class MongoDBConstant {
+
+    public static final String DATABASE_NAME = "testkidsdb";
+    public static final String HOST_NAME = "localhost";
+    public static final int PORT = 27017;
+}
---
+++ KidsMongoDao/src/test/java/com/kids/dao/errorinfo/ErrorInfoRepositoryImplTest.java
@@ -0,0 +1,122 @@
+package com.kids.dao.errorinfo;
+
+import com.kids.KidsMongoDaoApplication;
+import com.kids.dao.model.ErrorInfo;
+import com.kids.dao.repository.ErrorInfoRepository;
+import org.joda.time.LocalDateTime;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = KidsMongoDaoApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ErrorInfoRepositoryImplTest {
+
+    @Autowired
+    public ErrorInfoRepository errorInfoRepository;
+
+    @Test
+    public void case_01_findAll() {
+        assertEquals(3,errorInfoRepository.findAll().size());
+    }
+
+    @Test
+    public void case_02_findByErrorCode_not_found_data() {
+        assertNull(errorInfoRepository.findByErrorCode("10009"));
+    }
+
+    @Test
+    public void case_03_findByErrorCode_found_data() {
+        assertEquals("10001",errorInfoRepository.findByErrorCode("10001").getErrorCode());
+    }
+
+    @Test
+    public void case_04_update_success() {
+
+        ErrorInfo errorInfo = errorInfoRepository.findByErrorCode("10001");
+
+        Date oldDate = errorInfo.getModifyDate();
+        String oldModifyBy = errorInfo.getModifyBy();
+        errorInfo.setErrorMessageTh("ทดสอบ");
+        errorInfo.setErrorMessageEn("test");
+        errorInfo.setModifyBy("Admin");
+        errorInfo.setModifyDate(LocalDateTime.now().toDate());
+
+        ErrorInfo result = errorInfoRepository.update(errorInfo);
+
+        assertEquals(result.getErrorMessageEn(),errorInfo.getErrorMessageEn());
+        assertEquals(result.getErrorMessageTh(),errorInfo.getErrorMessageTh());
+        assertNotEquals(result.getModifyDate(),oldDate);
+        assertNotEquals(result.getModifyBy(),oldModifyBy);
+
+    }
+
+    @Test
+    public void case_05_save_success() {
+
+        int before = errorInfoRepository.findAll().size();
+
+        ErrorInfo errorInfo = new ErrorInfo();
+        errorInfo.setErrorCode("10003");
+        errorInfo.setErrorMessageEn("User role not authorized");
+        errorInfo.setErrorMessageTh("กลุ่มผู้ใช้งานไม่มีสิทธิใช้งานบริการ");
+        errorInfo.setCreateDate(LocalDateTime.now().toDate());
+        errorInfo.setCreateBy("Admin");
+
+        errorInfoRepository.save(errorInfo);
+
+        int after = errorInfoRepository.findAll().size();
+
+        assertEquals(before + 1,after);
+
+    }
+
+    @Test(expected = Exception.class)
+    public void case_06_save_fail_with_duplicates_key() {
+
+        int before = errorInfoRepository.findAll().size();
+
+        ErrorInfo errorInfo = new ErrorInfo();
+        errorInfo.setErrorCode("10003");
+        errorInfo.setErrorMessageEn("User role not authorized");
+        errorInfo.setErrorMessageTh("กลุ่มผู้ใช้งานไม่มีสิทธิใช้งานบริการ");
+        errorInfo.setCreateDate(LocalDateTime.now().toDate());
+        errorInfo.setCreateBy("Admin");
+
+        errorInfoRepository.save(errorInfo);
+
+        int after = errorInfoRepository.findAll().size();
+
+        assertEquals(before + 1,after);
+
+    }
+
+    @Test
+    public void case_07_delete_success() {
+
+        int before = errorInfoRepository.findAll().size();
+
+        ErrorInfo errorInfo = errorInfoRepository.findByErrorCode("10003");
+
+        errorInfoRepository.delete(errorInfo);
+
+        int after = errorInfoRepository.findAll().size();
+
+        assertEquals(before - 1,after);
+    }
+
+    @Test
+    public void case_08_delete_fail_with_null_obj() {
+        errorInfoRepository.delete(null);
+    }
+
+}

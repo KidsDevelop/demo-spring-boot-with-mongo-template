9fafde88c0f7edee4c80613deaafeb10f7c9f9f8
Kid Develop <kawee.l@g-able.com>
2017-03-08 14:14:43 +0700

    add resuorce file

--- KidsMongoDao/src/test/resources/errorInfo.sh
+++ KidsMongoDao/src/test/resources/errorInfo.sh
@@ -2,53 +2,10 @@ mongo
 
 use testkidsdb
 
-db.createCollection("errorInfo",
-    {
-        validator : { $and:
-            [
-                { errorCode : { $type : "string" } },
-                { errorMessageTh : { $type : "string" } },
-                { errorMessageEn : { $type : "string" } },
-                { createDate : { $type : "date" } },
-                { createBy : { $type : "string" } }
-            ]
-        },
-        validationLevel: "moderate",
-        validationAction : "error"
-    }
-)
+db.createCollection("errorInfo")
 
 db.getCollection('errorInfo').createIndex(
     { errorCode : 1 },
     { name: "errorCode_asc" ,unique: true }
 )
-
-db.getCollection('errorInfo').insert([
-	{
-		errorCode : "10000",
-		errorMessageTh : "ระบบไม่สามารถใช้บริการได้ในขณะนี้กรุณาลองใหม่ภายหลัง",
-		errorMessageEn : "Service is not available at this moment. Please try again later.",
-		createDate : new Date(),
-		createBy : "Admin"
-	},
-	{
-		errorCode : "10001",
-		errorMessageTh : "มีผู้เข้าใช้งานด้วยชื่อผู้ใช้งานเดียวกัน",
-		errorMessageEn : "Invalid Session",
-		createDate : new Date(),
-        createBy : "Admin"
-	},
-	{
-		errorCode : "10002",
-		errorMessageTh : "ขออภัยไม่มีข้อมูลที่ต้องการค้นหา",
-		errorMessageEn : "Not found any data.",
-		createDate : new Date(),
-        createBy : "Admin"
-	}
-])
-
 exit
-
-#db.getCollection('errorInfo').remove({})
-#db.getCollection('errorInfo').drop()
-#db.dropDatabase()
--- KidsMongoDao/src/test/resources/userInfo.sh
+++ KidsMongoDao/src/test/resources/userInfo.sh
@@ -2,80 +2,10 @@ mongo
 
 use testkidsdb
 
-db.createCollection("userInfo",
-    {
-        validator : { $and:
-            [
-                { citizenID : { $type : "string" } },
-                { userName : { $type : "string" } },
-                { authCode : { $type : "string" } },
-                { firstName : { $type: "string" } },
-                { lastName : { $type: "string" } },
-                { phoneNo : { $type : "string" } },
-                {
-                    emailAddress : {
-                        $regex : /@mongodb\.com$/,
-                        $type : "string"
-                    }
-                },
-                {
-                    userStatus : {
-                        $in : [ NumberInt(1), NumberInt(2), NumberInt(3) ] ,
-                        $type : "int"
-                    }
-                },
-                { createDate : { $type : "date" } },
-                { createBy : { $type : "string" } }
-            ]
-        },
-        validationLevel: "moderate",
-        validationAction : "error"
-    }
-)
+db.createCollection("userInfo")
 
 db.getCollection('userInfo').createIndex(
     { citizenID : 1 },
     { name: "citizenID_asc" ,unique: true }
 )
-
-db.getCollection('userInfo').insert([
-	{
-		citizenID : "9385739109836",
-		userName : "Joke",
-		authCode : "0192967Spring",
-		firstName : "Javax",
-		lastName : "Spring",
-		phoneNo : "02947164648",
-		emailAddress : "javax_Spring@mongodb.com",
-		userStatus : NumberInt(1),
-		createDate : new Date(),
-        createBy : "Admin"
-	},
-	{
-        citizenID : "9385739109777",
-        userName : "Jav",
-        authCode : "0192967Mongo",
-        firstName : "Javax",
-        lastName : "Mongo",
-        phoneNo : "02947164777",
-        emailAddress : "javax_Mongo@mongodb.com",
-        userStatus : NumberInt(1),
-        createDate : new Date(),
-        createBy : "Admin"
-    }
-])
-
 exit
-
-
-#db.getCollection('userInfo').updateMany({},
-#    {
-#        "$currentDate": { "createDate": {"$type": "date" } },
-#        "$set":{"createBy" : "Admin"}
-#    }
-#)
-
-#db.getCollection('userInfo').remove({})
-#db.getCollection('userInfo').drop()
-#db.dropDatabase()
-
